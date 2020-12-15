d46168f52b9b6e30faace555c5019016d75b8791
A Faruk Gonullu <af@gonul.lu>
2020-12-15 11:13:44 +0300

    fix: change relation carrying info

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -43,7 +43,7 @@ blogsRouter.post("/", async (request, response, next) => {
     author: body.author,
     url: body.url,
     likes: body.likes | 0,
-    user: user._id,
+    user: user,
   })
 
   const savedBlog = await blog.save()
